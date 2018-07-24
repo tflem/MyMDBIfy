49857f6e0624663aeeec64918e8d49f4d3024e41
Tim Fleming <tfleming26@hotmail.com>
2018-07-24 09:21:55 -0600

    Create movie detail view

--- django/core/views.py
+++ django/core/views.py
@@ -1,6 +1,10 @@
-from django.views.generic import ListView
-
+from django.views.generic import (
+    ListView, DetailView,
+)
 from core.models import Movie
+
+class MovieDetail(DetailView)
+    model = Movie
 
 class MovieList(ListView):
     model = Movie
