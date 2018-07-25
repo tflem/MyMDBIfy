055c5ae445257edb03ffcf00acfac056cc28ba44
Tim Fleming <tfleming26@hotmail.com>
2018-07-25 08:15:53 -0600

    Update model with ordering attribute and additional moving rating

--- django/core/models.py
+++ django/core/models.py
@@ -3,14 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Movie(models.Model):
-    NOT_RATED = 0
-    RATED_G = 1
-    RATED_PG = 2
-    RATED_R = 3
-    RATINGS = (
+    NOT_RATED   = 0
+    RATED_G     = 1
+    RATED_PG    = 2
+    RATED_PG_13 = 3
+    RATED_R     = 4
+    RATINGS     = (
         (NOT_RATED, 'NR - Not Rated'),
         (RATED_G, 'G - General Audiences'),
         (RATED_PG, 'PG - Parental Guidance ' 'Suggested'),
+        (RATED_PG_13, 'Parents Strongly Cautioned '
+        'Material May Be Inappropriate for children under 13.'),
         (RATED_R, 'R - Restricted'),
     )
 
@@ -20,6 +23,9 @@ class Movie(models.Model):
     rating  = models.IntegerField(choices=RATINGS, default=NOT_RATED)
     runtime = models.PositiveIntegerField()
     website = models.URLField(blank=True)
+
+    class Meta:
+        ordering = ('-year', 'title')
 
     def __str__(self):
         return '{} ({})'.format(self.title, self.year)
